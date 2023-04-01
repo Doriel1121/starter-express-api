a57e7834f3778db764e304cc4bc5e45b7da02dfe
Doriel aboya <doriel494@gmail.com>
2023-04-01 14:08:13 +0300

    first

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb("dull-gold-gazelle-bootCyclicDB")
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
+console.log(process.env.PORT);
+const data = db.collection("Approved");
+let attendence = data.set("Timna", {
+    amount: "2",
+    isComming:true
+})
+// console.log(data);
